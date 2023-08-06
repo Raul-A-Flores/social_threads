cd8738fa9a1d60b50c39da2a4801d083cd03a426
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-08-06 14:24:31 -0700

    suggested users

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -47,7 +47,7 @@ function LeftSidebar(){
             </div>
             <div className='mt-10 px-6'>
                 <SignedIn>
-                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                <SignOutButton signOutCallback={() => router.push("/sign-in")}>
                         <div className='flex cursor-pointer gap-4 p-4'>
                             <Image 
                                 src='/assets/logout.svg'
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -11,7 +11,7 @@ function Topbar(){
 
 
         <nav className="topbar">
-            <Link href='/' className='flex itesm-center gap-4'>s
+            <Link href='/' className='flex items-center gap-4'>s
                 <Image alt='logo' src='/assets/logo.svg' width={28} height={28} />
                     <p className='text-heading3-bold text-light-1 max-xs:hidden'>
                         Threads

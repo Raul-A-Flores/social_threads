81096c50b7b4c2db63a6daf034f8486a3b388354
Raul-A-Flores <raul.alexander.floress@gmail.com>
2023-08-06 05:12:08 -0700

    thread actions

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -25,6 +25,7 @@ import { useUploadThing } from '@/lib/uploadthing'
 import { updateUser } from '@/lib/actions/user.actions';
 import { usePathname, useRouter } from 'next/navigation'
 import { ThreadValidation } from '@/lib/validations/thread';
+import { createThread } from '@/lib/actions/thread.actions';
 
 interface Props {
     user: {
@@ -57,7 +58,10 @@ function PostThread({ userId}: { userId: string}){
           });
 
 
-    const onSubmit = () =>{
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) =>{
+        await createThread({text: values.thread, author: userId, communityId: null, path: pathname});
+
+        router.push('/')
 
     }
     return( 
